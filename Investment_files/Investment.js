76490946b41cae86a7b810afe60552981247c294
Jenny Hobbs <jennyhobbs@gmail.com>
2013-06-24 09:11:54 -0600

    updated site

--- Investment_files/Investment.js
+++ Investment_files/Investment.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20130615
+// Created by iWeb 3.0.4 local-build-20130624
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Investment_files/InvestmentMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
