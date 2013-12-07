efebdbcc5be262e93d27bb46741167c9b56fffa5
Jenny Hobbs <jennyhobbs@gmail.com>
2013-12-07 12:17:49 -0700

    updates

--- Photos_files/Photos.js
+++ Photos_files/Photos.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20130615
+// Created by iWeb 3.0.4 local-build-20130930
 
 function createMediaStream_id3()
 {return IWCreatePhotocast("http://www.jennyhobbs.com/jennyhobbs.com/Photos_files/rss.xml",true);}
